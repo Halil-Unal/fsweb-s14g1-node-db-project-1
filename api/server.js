bb9b62889c3b8e392d92eed2da8c0cff3ed8899f
HALİL ÜNAL <125642299+Halil-Unal@users.noreply.github.com>
2023-05-22 19:23:07 +0300

    ff

--- api/server.js
+++ api/server.js
@@ -3,5 +3,8 @@ const express = require("express");
 const server = express();
 
 server.use(express.json());
+const routers = require("./accounts/accounts-router");
+
+server.use("/api/accounts",routers)
 
 module.exports = server;
